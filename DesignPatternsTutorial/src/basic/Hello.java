ee979932c72a8001970c93275194d81af395f2f2
Somsubhra Ghosh <som.subhra@gmail.com>
2017-03-20 17:53:27 -0500

    testing commit from home

--- DesignPatternsTutorial/src/basic/Hello.java
+++ DesignPatternsTutorial/src/basic/Hello.java
@@ -2,14 +2,11 @@ package basic;
 
 public class Hello {
 
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello");
-		System.out.println("World");
-		
-		
-		
-
+		System.out.println("World!!");
 	}
 
 }
